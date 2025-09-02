d32bcc767358877c0f05ec6d2178ac76361a46a0
dkreise <140710823+dkreise@users.noreply.github.com>
2025-09-02 18:54:53 +0000

    nnf

---
+++ inc/Node.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <iostream>
+
+class Node {
+    public:
+        char value;
+        Node* left;
+        Node* right;
+
+        Node(char val, Node* l = nullptr, Node* r = nullptr) : value(val), left(l), right(r) {}
+
+        ~Node() {
+            delete left;
+            delete right;
+        }
+
+        void printTree(int space = 0) {
+            // if (this == nullptr) {
+            //     return;
+            // }
+            space += 4;
+            if (right) {
+                right->printTree(space);
+            }
+            // std::cout << std::endl;
+            for (int i = 0; i < space; i++) {
+                std::cout << " ";
+            }
+            std::cout << value << "\n";
+            if (left) {
+                left->printTree(space);
+            }
+        }
+
+        void printRPN() {
+            if (left) {
+                left->printRPN();
+            }
+            if (right) {
+                right->printRPN();
+            }
+            std::cout << value;
+        }
+};
--- main.cpp
+++ main.cpp
@@ -11,5 +11,10 @@ int main() {
     tests::test_eval_formula();
     tests::test_truth_table();
 
+    rsb::negation_normal_form("PQ>!R|");
+    rsb::negation_normal_form("AB&!");
+    rsb::negation_normal_form("AB|!");
+    rsb::negation_normal_form("P!QR!&=P>");
+
     return 0;
 }
