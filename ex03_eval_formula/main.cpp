a7afbf0f180fad87725242cc5169f4b3ed8f590d
Diana Kreise <dia.kreis@gmail.com>
2025-09-08 13:14:15 +0200

    eval formula done

--- ex03_eval_formula/main.cpp
+++ ex03_eval_formula/main.cpp
@@ -15,19 +15,39 @@ void print_test(const std::string& formula) {
 }
 
 int main() {
-    std::cout << std::endl << "----- 04 EVAL FORMULA -----" << std::endl;
+    std::cout << std::endl << "----- 04 EVAL FORMULA -----" << std::endl << std::endl;
 
-    print_test("1");
-    print_test("10&");
-    print_test("10|");
-    print_test("11>");
-    print_test("10=");
-    print_test("1011||=");
-    print_test("1011||=0>");
-    print_test("!0");
-    print_test("1>10");
-    print_test("02+");
-    print_test("1!1|0");
+    std::vector<std::string> formulas = {
+        "1",
+        "10&",
+        "10|",
+        "11>",
+        "10=",
+        "1011||=",
+        "1011||=0>",
+        "!0",
+        "1>10",
+        "02+",
+        "1!1|0",
+        "AB&!C!|"
+    };
+
+    std::cout << std::left << std::setw(20) << "Formula";
+    std::cout << std::left << std::setw(40) << "Result" << std::endl;
+    std::cout << std::string(60, '-') << std::endl;
+
+    for (const auto& formula : formulas) {
+        std::cout << std::left << std::setw(20) << formula;
+        try {
+            bool result = rsb::eval_formula(formula);
+            std::cout << std::left << std::setw(40) << (result ? "True" : "False") << std::endl;
+        }  catch (const std::invalid_argument& e) {
+            std::cout << std::left << std::setw(40) << e.what() << std::endl;
+        }
+        
+        
+    }
     
+    std::cout << std::endl;
     return 0;
 }
