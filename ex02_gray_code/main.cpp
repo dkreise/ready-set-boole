9be31f7f35915bc6b203f781754c0e93f934b333
Diana Kreise <dia.kreis@gmail.com>
2025-09-08 13:04:32 +0200

    gray code done

--- ex02_gray_code/main.cpp
+++ ex02_gray_code/main.cpp
@@ -2,18 +2,22 @@
 
 void check_gray_code(unsigned int n, unsigned int expected) {
     unsigned int result = rsb::gray_code(n);
-    std::cout << " " << n << " (gray) = " << result << " (expected: " << expected << ")" << std::endl;
+    std::cout << "Gray( " << n << " ) = " << result << " (expected: " << expected << ")" << std::endl;
 }
 
 int main() {
-    std::cout << std::endl << "----- 03 GRAY CODE -----" << std::endl;
+    std::cout << std::endl << "----- 03 GRAY CODE -----" << std::endl << std::endl;
 
     check_gray_code(0, 0);
     check_gray_code(1, 1);
     check_gray_code(2, 3);
     check_gray_code(3, 2);
-    check_gray_code(4, 5);
-    check_gray_code(5, 4);
+    check_gray_code(4, 6);
+    check_gray_code(5, 7);
+    check_gray_code(6, 5);
+    check_gray_code(7, 4);
+    check_gray_code(8, 12);
 
+    std::cout << std::endl;
     return 0;
 }
