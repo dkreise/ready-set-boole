425cae2b59321cb301a27a6abf49af6aadecd40e
Diana Kreise <dia.kreis@gmail.com>
2025-09-14 12:57:54 +0200

    added try catch to tests

--- ex/ex06_cnf/main.cpp
+++ ex/ex06_cnf/main.cpp
@@ -1,4 +1,4 @@
-#include "../rsb.hpp"
+#include "../../rsb.hpp"
 
 int main() {
     std::cout << std::endl << "----- 06 CNF -----" << std::endl << std::endl;
@@ -26,10 +26,14 @@ int main() {
     std::cout << std::string(60, '-') << std::endl;
 
     for (const auto& formula : formulas) {
-        std::string result = rsb::conjunctive_normal_form(formula);
-
         std::cout << std::left << std::setw(20) << formula;
-        std::cout << std::left << std::setw(40) << result << std::endl;
+        
+        try {
+            std::string result = rsb::conjunctive_normal_form(formula);
+            std::cout << std::left << std::setw(40) << result << std::endl;
+        }  catch (const std::invalid_argument& e) {
+            std::cout << std::left << std::setw(40) << e.what() << std::endl;
+        }
     }
 
     std::cout << std::endl;
